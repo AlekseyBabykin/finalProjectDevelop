ef502534fd9d7adfcc7d75b57c772fb73f7375ff
Aleksey <a.babykiny@gmail.com>
2023-11-04 18:00:14 +0200

    URL

--- client_server/server/client/src/http/index.js
+++ client_server/server/client/src/http/index.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 const $host = axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "http://localhost:5000/",
+  baseURL: "/",
 });
 
 const $authHost = axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: " http://localhost:5000/",
+  baseURL: "/",
 });
 
 const authInterceptor = (config) => {
